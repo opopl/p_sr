6d29f32baead3a1ed0d8a7c01afa4bcddfabb1f6
Alexander Poplavskiy <Alexander.Poplavskiy@dataart.com>
2020-08-22 22:04:31 +0300

    u

---
+++ letopis.fig.16_07_2020.news.dnr.tri_color_center.1.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/env perl 
+
+use strict;
+use warnings;
+use utf8;
+
+use Data::Dumper qw(Dumper);
+
+use DBI;
+
+my $tags = qw(tags_fig 16_07_2020.news.dnr.tri_color_center.1);
+
+print "xxxxxxxxxxxxxx" . "\n";
+print "zzzzzzzzzzzzzz" . "\n";
+print "zzzzzzzzzzzzzz" . "\n";
+

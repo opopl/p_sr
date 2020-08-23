3268b2cc1654fd5cc8e2beb1680a7a18c2fc89c9
Alexander Poplavskiy <Alexander.Poplavskiy@dataart.com>
2020-08-23 21:14:19 +0300

    u

--- letopis.fig.16_07_2020.news.dnr.tri_color_center.1.pl
+++ letopis.fig.16_07_2020.news.dnr.tri_color_center.1.pl
@@ -5,12 +5,19 @@ use warnings;
 use utf8;
 
 use Data::Dumper qw(Dumper);
+use FindBin qw($Bin $Script);
+use File::Basename qw(basename dirname);
+
+my ($proj)  = ($Script =~ m/^(\w+)\..*$/);
+my $root_id = basename($Bin);
+my $root    = $Bin;
 
 use Plg::Projs::Piwigo::SQL;
 
 my @tags_base;
 push @tags_base,
-    qw(projs letopis p_saintrussia),
+    qw(projs),
+    ($proj, $root_id),
     qw(16_07_2020.news.dnr.tri_color_center.1)
     ;
 
@@ -20,7 +27,11 @@ my $pwg = Plg::Projs::Piwigo::SQL->new;
 
 my @tex_lines;
 
-push @tex_lines,q{\begin{tabular}{ccc}};
+my $cols = 3;
+
+push @tex_lines,sprintf(q{\begin{tabular}{%s}},'c' x $cols);
+
+my $w = 0.3;
 foreach my $num (@range) {
     my @tags;
 
@@ -35,8 +46,20 @@ foreach my $num (@range) {
 
     my @img = @{$pwg->{img} || []};
     my $first = shift @img;
+
+    my $rel_path = $first->{rel_path};
+
+    my $eol = ( $num % $cols == 0 ) ? q{\\\\} : q{&};
+
+    push @tex_lines,
+        sprintf(q{\def\picpath{\imgroot/%s}},$rel_path),
+        sprintf(q|\def\picopts{width=%s\textwidth}|,$w),
+        sprintf(q{\includegraphics[\picopts]{\picpath}}),
+        $eol,
+        '%' . 'x' x 50,
+        ;
 }
-push @tex_lines,q{\end{tabular}{ccc}};
+push @tex_lines,q{\end{tabular}};
 
 my $tex = join("\n",@tex_lines);
 print $tex . "\n";

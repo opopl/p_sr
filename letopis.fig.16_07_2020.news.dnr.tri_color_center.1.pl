4d2dfdf58c776e2d6cfb11b81626e3cc417c37e4
Alexander Poplavskiy <Alexander.Poplavskiy@dataart.com>
2020-08-23 20:47:29 +0300

    u

--- letopis.fig.16_07_2020.news.dnr.tri_color_center.1.pl
+++ letopis.fig.16_07_2020.news.dnr.tri_color_center.1.pl
@@ -6,11 +6,37 @@ use utf8;
 
 use Data::Dumper qw(Dumper);
 
-use DBI;
+use Plg::Projs::Piwigo::SQL;
 
-my $tags = qw(tags_fig 16_07_2020.news.dnr.tri_color_center.1);
+my @tags_base;
+push @tags_base,
+    qw(projs letopis p_saintrussia),
+    qw(16_07_2020.news.dnr.tri_color_center.1)
+    ;
 
-print "xxxxxxxxxxxxxx" . "\n";
-print "zzzzzzzzzzzzzz" . "\n";
-print "zzzzzzzzzzzzzz" . "\n";
+my @range = ( 1 .. 6 );
 
+my $pwg = Plg::Projs::Piwigo::SQL->new;
+
+my @tex_lines;
+
+push @tex_lines,q{\begin{tabular}{ccc}};
+foreach my $num (@range) {
+    my @tags;
+
+    push @tags,
+        @tags_base,$num;
+
+    local @ARGV = qw( -c img_by_tags );
+    push @ARGV, 
+        qw( -t ), join("," => @tags);
+
+    $pwg->run;
+
+    my @img = @{$pwg->{img} || []};
+    my $first = shift @img;
+}
+push @tex_lines,q{\end{tabular}{ccc}};
+
+my $tex = join("\n",@tex_lines);
+print $tex . "\n";

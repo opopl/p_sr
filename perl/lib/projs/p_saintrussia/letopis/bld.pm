df93658929faf4deebf7bf692ebb419f38993c05
Alexander Poplavskiy <Alexander.Poplavskiy@dataart.com>
2020-08-26 10:31:23 +0300

    u

--- perl/lib/projs/p_saintrussia/letopis/bld.pm
+++ perl/lib/projs/p_saintrussia/letopis/bld.pm
@@ -7,6 +7,8 @@ use warnings;
 use base qw(
     Plg::Projs::Prj
 );
+use FindBin qw($Bin $Script);
+
 use File::Spec::Functions qw(catfile);
  
 use Plg::Projs::Scripts::RunPdfLatex;
@@ -16,7 +18,17 @@ sub init {
 
     $self->SUPER::init();
 
-
+    unless (@ARGV) {
+        print qq{
+            LOCATION:
+                $0
+            USAGE:
+                $Script ACT
+        } . "\n";
+        exit 1;
+    }
+    my $act = shift @ARGV || 'build';
+    $self->{act} = $act; 
 
     my $h = {
     };
@@ -34,18 +46,26 @@ sub init_maker {
     my ($self) = @_;
 
     my @secs_include;
-    push @secs_include,
+    #push @secs_include,
         #'13_07_2020',
         #'14_07_2020',
         #'15_07_2020',
         ;
+
+    my $maps_act = {
+        'build' => 'build_pwg',
+        'join'  => 'insert_pwg',
+    };
+
+    my $act = $self->{act};
+    my $cmd = $maps_act->{$act} || '';
 
     my $x = Plg::Projs::Build::PdfLatex->new(
         skip_get_opt => 1,
         proj         => $self->{proj},
         root         => $self->{root},
         root_id      => $self->{root_id},
-        cmd          => 'build_pwg',
+        cmd          => $cmd,
         join_lines   => {
             include_below => [qw(section)]
         },

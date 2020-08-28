cdb7b3c9aaf99bdd418d562321180522be1cb1af
Alexander Poplavskiy <Alexander.Poplavskiy@dataart.com>
2020-08-28 21:53:46 +0300

    u

--- perl/lib/projs/p_saintrussia/letopis/bld.pm
+++ perl/lib/projs/p_saintrussia/letopis/bld.pm
@@ -11,6 +11,7 @@ use FindBin qw($Bin $Script);
 use Data::Dumper qw(Dumper);
 
 use File::Spec::Functions qw(catfile);
+use Getopt::Long qw(GetOptions);
  
 use Plg::Projs::Scripts::RunPdfLatex;
 
@@ -19,62 +20,104 @@ sub init {
 
     $self->SUPER::init();
 
+    my $h = {
+        maps_act => {
+            'compile' => 'build_pwg',
+            'join'    => 'insert_pwg',
+        },
+        act_default => 'compile',
+    };
+    my @acts = sort keys %{$h->{maps_act} || {}};
+
+    my @k = keys %$h;
+
+    for(@k){ $self->{$_} = $h->{$_} unless defined $self->{$_}; }
+
+    my $acts_s = join(" ",@acts);
     unless (@ARGV) {
         print qq{
             LOCATION:
                 $0
             USAGE:
-                $Script ACT
+                $Script ACT -c CONFIG
+            ACTS:
+                $acts_s
+            DEFAULT ACT:
+                $self->{act_default}
+            EXAMPLES:
+                $Script compile -c 'xelatex'
         } . "\n";
         exit 1;
     }
     my $act = shift @ARGV || 'compile';
     $self->{act} = $act; 
 
-    my $h = {
-    };
-        
-    my @k = keys %$h;
+    $self->get_opt;
 
-    for(@k){ $self->{$_} = $h->{$_} unless defined $self->{$_}; }
 
     $self->init_maker;
 
     return $self;
 }
 
-sub _insert_titletoc {
-	my $self = shift;
+sub _config_set {
+    my ($self, $cfg) = @_;
 
-	my @d;
-	push @d,
-			{
+    grep { /^$cfg$/ } @{$self->{config} || []}; 
+
+}
+
+      
+sub get_opt {
+    my ($self) = @_;
+    
+    Getopt::Long::Configure(qw(bundling no_getopt_compat no_auto_abbrev no_ignore_case_always));
+    
+    my %opt;
+    my @optstr = ( 
+        "config|c=s",
+    );
+
+    GetOptions(\%opt,@optstr);
+    
+    $self->{config} = split(',' => ($opt{config} || ''));
+
+    return $self;   
+}
+
+
+sub _insert_titletoc {
+    my $self = shift;
+
+    my @d;
+    push @d,
+            {
 ###ttt_section
              scts    => [qw( section )],
              lines => [
                  ' ',
                  '\startcontents[subsections]',
- 	'\printcontents[subsections]{l}{2}{\addtocontents{ptc}{\setcounter{tocdepth}{3}}}',
+    '\printcontents[subsections]{l}{2}{\addtocontents{ptc}{\setcounter{tocdepth}{3}}}',
 
              ],
              lines_stop => [
                  '\stopcontents[subsections]',
- 			]
- 		},
- 		{
+            ]
+        },
+        {
              scts    => [qw( chapter )],
              lines => [
                  ' ',
                  '\startcontents[sections]',
- 	'\printcontents[sections]{l}{1}{\addtocontents{ptc}{\setcounter{tocdepth}{1}}}',
+    '\printcontents[sections]{l}{1}{\addtocontents{ptc}{\setcounter{tocdepth}{1}}}',
                  ' ',
              ],
              lines_stop => [
                  '\stopcontents[sections]',
- 			]
- 		},
-		;
-	return [@d];
+            ]
+        },
+        ;
+    return [@d];
 }
 
 sub init_maker {
@@ -87,13 +130,9 @@ sub init_maker {
         #'15_07_2020',
         ;
 
-    my $maps_act = {
-        'compile' => 'build_pwg',
-        'join'    => 'insert_pwg',
-    };
 
     my $act = $self->{act};
-    my $cmd = $maps_act->{$act} || '';
+    my $cmd = $self->{maps_act}->{$act} || '';
 
 
     local @ARGV = ();
@@ -110,9 +149,9 @@ sub init_maker {
             include => \@secs_include,
             insert => {
                 titletoc => $self->_insert_titletoc,
-				bookmarks => [
-					scts => [qw(section)],
-				]
+                bookmarks => [
+                    scts => [qw(section)],
+                ]
             },
         }
     );

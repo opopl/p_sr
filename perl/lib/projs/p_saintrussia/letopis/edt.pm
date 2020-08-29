e6f69267a9c96b492b6690997aec7b340247786a
Alexander Poplavskiy <Alexander.Poplavskiy@dataart.com>
2020-08-29 22:58:03 +0300

    u

--- perl/lib/projs/p_saintrussia/letopis/edt.pm
+++ perl/lib/projs/p_saintrussia/letopis/edt.pm
@@ -20,6 +20,92 @@ use base qw(
 );
 use Data::Dumper qw(Dumper);
 
+sub _sub_process_file {
+    my ($self, $ref) = @_;
+	
+	my $sec = $ref->{sec};
+	
+	if($sec =~ m/^(?<day>\d+)_(?<month>\d+)_(?<year>\d+)$/g){
+	    $ref->{date_sec} = {%+};
+	
+	    my @date;
+	    push @date, 
+	        @+{qw(year month day)},
+	        qw( 0 0 0 )
+	        ;
+	
+        my $dt = $self->{dt};
+	    $dt->set('date',\@date);
+	
+	    my $str = eval { $dt->printf("%d %B %Y, %A"); };
+	
+	    if ($str) {
+	        $ref->{date_str} = $str;
+	    }
+	}
+	
+	return $ref;
+}
+
+sub _sub_edit_line {
+    my $self = shift;
+
+    local $_ = shift;
+
+    #s/^\s*//g;
+    #s/\s*$//g;
+
+    my ($ref,$run) = @_;
+
+    my $re = $self->{re};
+
+    s/(\s+)–(\s+)/$1---$2/g;
+    s/(\d+)–(\d+)/$1-$2/g;
+
+    s/index\.cities\.rus/cities.rus/g;
+    #s/index\.names\.rus/names.rus/g;
+    #s/index\.authors\.rus/authors.rus/g;
+    #s/index\.rus/rus/g;
+    #s/index\.eng/eng/g;
+
+    my $date_sec = $ref->{date_sec} || {};
+    my $is_date = keys %$date_sec ? 1 : 0;
+
+    my $sec = $ref->{sec};
+
+    if (/$re->{sec}/) {
+        return $_ if $run->{sec_done};
+
+        my @sec_plus; 
+
+        push @sec_plus, 
+               sprintf(q{\label{sec:%s}},$sec),
+               ;
+
+        my $sec_plus = join("\n",@sec_plus);
+
+        my $secname  = $+{secname};
+        my $sectitle = $+{sectitle};
+
+        my $date_str = $ref->{date_str};
+
+        my $new_sec = ($is_date && $date_str) ? $date_str : $sectitle;
+
+        s/$re->{sec}/\\$secname\{$new_sec\}\n$sec_plus/g;
+
+        $run->{sec_done} = 1;
+
+        return $_;
+
+    }
+
+    s/^\\label\{sec:$sec\}.*//g;
+    s/^\s*%edt\s*\n\s*\n//g;
+    
+    return $_;
+
+}
+
 sub init {
     my ($self) = @_;
 
@@ -30,87 +116,9 @@ sub init {
     $dt->config('Language' => 'russian');
 
     my $h = {
-        subs => {
-            process_file => sub {
-                my ($ref) = @_;
-
-                my $sec = $ref->{sec};
-
-                if($sec =~ m/^(?<day>\d+)_(?<month>\d+)_(?<year>\d+)$/g){
-                    $ref->{date_sec} = {%+};
-
-                    my @date;
-                    push @date, 
-                        @+{qw(year month day)},
-                        qw( 0 0 0 )
-                        ;
-
-                    $dt->set('date',\@date);
-
-                    my $str = eval { $dt->printf("%d %B %Y, %A"); };
-
-                    if ($str) {
-                        $ref->{date_str} = $str;
-                    }
-                }
-
-                return $ref;
-            },
-            edit_line => sub {
-                local $_ = shift;
-
-                #s/^\s*//g;
-                #s/\s*$//g;
-
-                my ($ref,$run) = @_;
-
-                s/(\s+)–(\s+)/$1---$2/g;
-                s/(\d+)–(\d+)/$1-$2/g;
-    
-#                s/index\.cities\.rus/cities.rus/g;
-                #s/index\.names\.rus/names.rus/g;
-                #s/index\.authors\.rus/authors.rus/g;
-                #s/index\.rus/rus/g;
-                #s/index\.eng/eng/g;
-
-                my $date_sec = $ref->{date_sec} || {};
-                my $is_date = keys %$date_sec ? 1 : 0;
-
-                my $sec = $ref->{sec};
-
-                if (/$re->{sec}/) {
-                    return $_ if $run->{sec_done};
-
-                    my @sec_plus; 
-
-                    push @sec_plus, 
-                           sprintf(q{\label{sec:%s}},$sec),
-                           ;
-
-                    my $sec_plus = join("\n",@sec_plus);
-
-                    my $secname  = $+{secname};
-                    my $sectitle = $+{sectitle};
-
-                    my $date_str = $ref->{date_str};
-
-                    my $new_sec = ($is_date && $date_str) ? $date_str : $sectitle;
-    
-                    s/$re->{sec}/\\$secname\{$new_sec\}\n$sec_plus/g;
-
-                    $run->{sec_done} = 1;
-
-                    return $_;
-    
-                }
-
-                s/^\\label\{sec:$sec\}.*//g;
-                s/^\s*%edt\s*\n\s*\n//g;
-                
-
-                return $_;
-            }
-        }
+        re => $re,
+        dt => $dt,
+        subs => { }
     };
         
     my @k = keys %$h;

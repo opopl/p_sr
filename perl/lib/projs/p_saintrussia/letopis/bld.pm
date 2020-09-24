eb5f62b65c1a0d05d88368af5c755e54c2748498
op <op@localhost>
2020-09-24 20:14:16 +0300

    u

--- perl/lib/projs/p_saintrussia/letopis/bld.pm
+++ perl/lib/projs/p_saintrussia/letopis/bld.pm
@@ -11,12 +11,32 @@ use File::Spec::Functions qw(catfile);
 use Getopt::Long qw(GetOptions);
 
 use Base::Arg qw(
-	hash_update
+    hash_update
 );
 
 use base qw(
-	Plg::Projs::Prj::Builder
+    Plg::Projs::Prj::Builder
 );
+
+use Base::Arg qw( hash_inject );
+
+sub init {
+    my ($self) = @_;
+    
+    my $h = {
+        tex_exe => 'pdflatex',
+        insert => { 
+            hyperlinks => 1,
+            hypertoc   => 1,
+        },
+    };
+    hash_inject($self, $h);
+    $self->SUPER::init();
+    
+    return $self;
+}
+
+
 
 1;
  

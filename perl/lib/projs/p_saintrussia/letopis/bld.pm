27f7218b852c1b97976cc172500cab8eccc339f7
Alexander Poplavskiy <Alexander.Poplavskiy@dataart.com>
2020-09-25 17:56:18 +0300

    u

--- perl/lib/projs/p_saintrussia/letopis/bld.pm
+++ perl/lib/projs/p_saintrussia/letopis/bld.pm
@@ -24,6 +24,7 @@ sub init {
     my ($self) = @_;
     
     my $h = {
+        trg_list => [qw(usual)],
         tex_exe => 'pdflatex',
         insert => { 
             hyperlinks => 1,
@@ -38,7 +39,32 @@ sub init {
             }
         }
     };
+
     hash_inject($self, $h);
+    $self->SUPER::init();
+
+    return $self;
+}
+
+sub init_trg_usual {
+    my ($self) = @_;
+    
+    my $h = {
+        tex_exe => 'pdflatex',
+        insert => { 
+            hyperlinks => 1,
+            titletoc   => 1,
+        },
+        opts_maker => {
+            load_dat => {
+                ii_include => 1,
+            },
+            sections => {
+                include => [],
+            }
+        }
+    };
+    hash_inject($self, { targets => { usual => $h }});
     $self->SUPER::init();
 
     return $self;

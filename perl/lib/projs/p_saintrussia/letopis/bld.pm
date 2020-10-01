4dd2a80d08e6506c8a36cf875711e930a3b85078
op <op@localhost>
2020-10-01 18:38:31 +0300

    u

--- perl/lib/projs/p_saintrussia/letopis/bld.pm
+++ perl/lib/projs/p_saintrussia/letopis/bld.pm
@@ -26,25 +26,6 @@ sub init {
     my $h = {
         trg_list => [qw(usual)],
         tex_exe => 'pdflatex',
-        insert => { 
-            hyperlinks => 1,
-            titletoc   => 1,
-        },
-        opts_maker => {
-            load_dat => {
-                ii_include => 0,
-            },
-            sections => {
-                include          => [qw(
-                    _base_
-                    defs
-                )],
-                include_with_children => [qw(
-                    preamble
-                    titlepage
-                )],
-            },
-        }
     };
 
     hash_inject($self, $h);
@@ -54,34 +35,4 @@ sub init {
     return $self;
 }
 
-#sub trg_inj_usual {
-    #my ($self) = @_;
-
-    #$self->SUPER::_trg_inj_usual();
-    
-    #my $h = {
-        #tex_exe => 'pdflatex',
-        #insert => { 
-            #hyperlinks => 1,
-            #titletoc   => 1,
-        #},
-        #opts_maker => {
-            #load_dat => {
-                #ii_include => 1,
-            #},
-            #sections => {
-                #include => [],
-            #}
-        #}
-    #};
-
-    #$self->trg_inject('usual' => $h);
-
-    #return $self;
-#}
-
-
-
 1;
- 
-

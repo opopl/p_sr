82497a980238d1b9ab608c16aaf9ba043e420ac8
op <op@localhost>
2020-10-16 07:23:48 +0300

    u

---
+++ istoria.bld.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/env perl
+
+use warnings;
+use strict;
+
+use base qw(
+    projs::p_sr::istoria::bld
+);
+
+__PACKAGE__->new->run;
---
+++ istoria.edt.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/env perl
+
+use warnings;
+use strict;
+
+use base qw(
+    projs::p_sr::istoria::edt
+);
+
+__PACKAGE__->new->run;

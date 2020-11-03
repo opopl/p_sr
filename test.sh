30ea3f9d64d42dc9789fb8f3eab21146790c2f9c
op <op@localhost>
2020-11-03 20:40:49 +0200

    u

---
+++ test.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+perl '/Users/oleksandrpoplavskiy/vrt/plg/projs/scripts/bufact/tex/list_img.pl' '/Users/oleksandrpoplavskiy/repos/git/p_sr/letopis.30_10_2020.news.ua.strana.2.maidan_dtp.tex'

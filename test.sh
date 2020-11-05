ca53585a88daff9030866134ecb1a17c962fb859
op <op@localhost>
2020-11-05 19:07:52 +0200

    u

--- test.sh
+++ test.sh
@@ -3,4 +3,6 @@
 export script="$PLG/projs/scripts/bufact/tex/list_img.pl"
 export file="$P_SR/letopis.30_10_2020.news.ua.strana.2.maidan_dtp.tex"
 
-perl $script -f $file
+#perl $script 
+perl $script -f $file -d
+#perl $script -p "letopis" -r "$P_SR"

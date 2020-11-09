db7fb3f9dfa00b1ff6cc8e9fa5ada2373986750a
op <op@localhost>
2020-11-09 19:19:19 +0200

    u

--- get_img.sh
+++ get_img.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-export script="$PLG/projs/scripts/bufact/tex/list_img.pl"
+export script="$PLG/projs/scripts/bufact/tex/get_img.pl"
 export file="$P_SR/letopis.30_10_2020.news.ua.strana.2.maidan_dtp.tex"
 
 #perl $script 
-perl $script -f $file -d
-#perl $script -p "letopis" -r "$P_SR"
+#perl $script -f $file -d
+#perl $script -p "letopis" -r "$P_SR" --reset
+perl $script -p "letopis" -r "$P_SR"

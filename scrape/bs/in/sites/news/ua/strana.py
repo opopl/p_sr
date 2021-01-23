34fb0f88a229e82ed09b021a57af678520907d9b
op <op@localhost>
2021-01-23 20:28:00 +0200

    u

--- scrape/bs/in/sites/news/ua/strana.py
+++ scrape/bs/in/sites/news/ua/strana.py
@@ -1,5 +1,6 @@
 
-import os,sys
+import os,sys,re
+import datetime
 
 def add_libs(libs):
   for lib in libs:
@@ -11,5 +12,20 @@ add_libs([ os.path.join(plg,'projs','python','lib') ])
 import Base.Util as util
 
 class Page:
+  soup = None
+  app = None
+  date_format = ''
+
+  def __init__(self,args={}):
+    for k, v in args.items():
+      setattr(self, k, v)
+
   def get_date(self,ref={}):
-    soup = util.get(ref,'soup')
+    e = self.soup.select_one('time.date span.strana-adate')
+    if e and e.has_attr('data-time'):
+      s = e['data-time']
+      f = "%Y-%m-%d %H:%M:%S"
+      d = datetime.datetime.strptime(s,f)
+      date = d.strftime('%d_%m_%Y')
+
+    return self

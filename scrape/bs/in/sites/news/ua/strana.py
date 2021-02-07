f59fd9afa56039347d37be0daba12be5c06bbbc4
op <op@localhost>
2021-02-07 10:04:32 +0200

    u

--- scrape/bs/in/sites/news/ua/strana.py
+++ scrape/bs/in/sites/news/ua/strana.py
@@ -15,14 +15,7 @@ from Base.Scraper.Author import Author
 
 class PageParser(RootPageParser):
 
-  def get_date(self,ref={}):
-    e = self.soup.select_one('time.date span.strana-adate')
-    if e and e.has_attr('data-time'):
-      s = e['data-time']
-      s = s.split(' ')[0]
-      f = "%Y-%m-%d"
-      d = datetime.datetime.strptime(s,f)
-      date = d.strftime('%d_%m_%Y')
-      self.app.page.set({ 'date' : date })
+  def get_date_html(self,ref={}):
+    super().get_date_html(ref)
 
     return self

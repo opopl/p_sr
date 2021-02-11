8b4217cfa3cd2fa9211df44087ac629b5c4a6402
op <op@localhost>
2021-02-11 16:01:44 +0200

    u

--- scrape/bs/in/sites/news/ua/pravda.py
+++ scrape/bs/in/sites/news/ua/pravda.py
@@ -10,8 +10,53 @@ import Base.Const as const
 from Base.Scraper.PageParser import RootPageParser
 
 class PageParser(RootPageParser):
+
+  month_map_genitive = {
+    'січня'     : '01',
+    'лютого'    : '02',
+    'березня'   : '03',
+    'квітня'    : '04',
+    'травня'    : '05',
+    'червня'    : '06',
+    'липня'     : '07',
+    'серпня'    : '08',
+    'вересня'   : '09',
+    'жовтня'    : '10',
+    'листопада' : '11',
+    'грудня'    : '12',
+  }
+
   def generate_ii(self,ref={}):
     super().generate_ii(ref)
+
+    return self
+
+  def get_author(self,ref={}):
+    super().get_author(ref)
+
+    app = self.app
+
+    if util.get(app.page,'author_id'):
+      return self
+
+    d_parse = {}
+
+    host = app.page.get('host','')
+    m = re.match(r'^blogs\.',host)
+    if m:
+      el = app.soup.select_one('.bauthor a')
+      if el:
+        str = el.string.strip()
+        d_parse['str'] = str
+        if el.has_attr('href'):
+          href = el['href']
+          u = util.url_parse(href)
+          if not u['netloc']:
+            href = util.url_join(app.page.baseurl, href)
+
+        d_parse['url'] = href
+
+    self.auth_obj.parse(d_parse)
 
     return self
 
@@ -22,6 +67,25 @@ class PageParser(RootPageParser):
 
     if util.get(app.page,'date'):
       return self
+
+    host = app.page.get('host','')
+    m = re.match(r'^blogs\.',host)
+    if m:
+      el = app.soup.select_one('.bdate')
+      if el:
+        date_s = el.string.strip().split(',')[0]
+        m = re.match(r'^(\d+)\s+(\w+)\s+(\w+)',date_s)
+        if m:
+          day       = m.group(1)
+          month_gen = m.group(2)
+          year      = m.group(3)
+  
+          month = self.month_map_genitive.get(month_gen,'')
+  
+          if month:
+            date = '_'.join([day,month,year])
+            app.page.set({ 'date' :  date })
+            return self
 
     m = re.match(r'^[/]*(\w+)/(\d{4})/(\d{2})/(\d{2})/',self.url_path)
     if m:

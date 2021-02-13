9566bbcaea10332bf2ebe2116f6d793857994db3
op <op@localhost>
2021-02-13 13:45:33 +0200

    u

--- scrape/bs/in/sites/news/ua/den.py
+++ scrape/bs/in/sites/news/ua/den.py
@@ -28,3 +28,23 @@ class PageParser(RootPageParser):
       return self
 
     return self
+
+  # import date from input string
+  def _date_from_bare(self,sel = {}):
+    date = super()._date_from_bare(sel)
+    if date:
+      return date
+
+    m = re.match(r'^(\d+)\s+(\w+),\s+(\d+)',self.date_bare)
+    if m:
+      day       = m.group(1)
+      month_gen = m.group(2)
+      year      = m.group(3)
+
+      month = self.month_map_genitive['ukr'].get(month_gen,'')
+  
+      if month:
+        date = '_'.join([day,month,year])
+
+    #date = None
+    return date

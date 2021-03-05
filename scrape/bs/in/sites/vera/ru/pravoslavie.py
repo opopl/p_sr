4f492d524243e397a2ab1eeed9acfead7e5bb355
op <op@localhost>
2021-03-05 15:51:10 +0200

    u

--- scrape/bs/in/sites/vera/ru/pravoslavie.py
+++ scrape/bs/in/sites/vera/ru/pravoslavie.py
@@ -22,21 +22,21 @@ class PageParser(RootPageParser):
     if util.get(app.page,'date'):
       return self
 
-    el = app.soup.select_one('.block-doc__date')
-    if el:
-      date_s = el.string.strip().split(',')[0]
-      m = re.match(r'^(\d+)\s+(\w+)\s+(\d+)',date_s)
-      if m:
-        day       = m.group(1)
-        month_gen = m.group(2)
-        year      = m.group(3)
+#    el = app.soup.select_one('.block-doc__date')
+    #if el:
+      #date_s = el.string.strip().split(',')[0]
+      #m = re.match(r'^(\d+)\s+(\w+)\s+(\d+)',date_s)
+      #if m:
+        #day       = m.group(1)
+        #month_gen = m.group(2)
+        #year      = m.group(3)
   
-        month = self.month_map_genitive['rus'].get(month_gen,'')
+        #month = self.month_map_genitive['rus'].get(month_gen,'')
   
-        if month:
-          date = '_'.join([day,month,year])
-          app.page.set({ 'date' :  date })
-          return self
+        #if month:
+          #date = '_'.join([day,month,year])
+          #app.page.set({ 'date' :  date })
+          #return self
 
     return self
 

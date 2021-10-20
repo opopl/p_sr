0db0d8de3c0184a1a0bb88919185f81079fafbe3
op <op@localhost>
2021-10-20 22:45:27 +0300

    u

---
+++ fb/m.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+from Base.Scraper.Models import mAuthors, mProjs
+import os,sys,re
+
+import Base.Util as util
+
+url = 'https://www.facebook.com/permalink.php?story_fbid=3032849666973660&id=100007459690216'
+#a = list(mAuthors.select().where(mAuthors.id.contains('oleg')).dicts())
+
+u = util.url_parse(url,{ 'rm_query' : 0 })
+
+a = list(mProjs.select().where(mProjs.url == url).dicts())
+
+import pdb; pdb.set_trace()

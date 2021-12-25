d6a1c1d41cf4594cd9cf2d6b0a18f27f8881a723
op <op@localhost>
2021-12-25 17:25:45 +0200

    u

--- gi.sh
+++ gi.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 export script="$PLG/projs/scripts/bufact/tex/get_img.pl"
+
 #export file="$P_SR/letopis.30_10_2020.news.ua.strana.2.maidan_dtp.tex"
 
 #perl $script -f $file -d
 #perl $script -p "letopis" -r "$P_SR" --reset
 #perl -d $script -p "letopis"
 
-perl $script -p "letopis"
-
+#perl $script -p "letopis"
 #export file="$P_SR/letopis.12_09_2021.fb.semesjuk_ivan.1.chelovek_prividenie.tex"
+
+export file="letopis.24_12_2021.stz.news.ru.kp.1.doneck_jolka.pic.1.tex"
 #perl -d $script -f $file
+perl $script -f $file

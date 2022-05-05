fa9bf9be13edcab48cbe148df95aebe6d47b9b5d
me <me@localhost>
2022-05-05 18:41:41 +0300

    u

--- b.sh
+++ b.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-perl letopis.bld.pl compile -t nov_2020
+#perl letopis.bld.pl compile -t nov_2020
+
+perl letopis.bld.pl compile -t vojna.harkov
+perl letopis.bld.pl compile -t poetry

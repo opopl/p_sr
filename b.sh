d43254620e319a82fa011ac45fd2592b9310a911
me <me@localhost>
2022-05-13 15:01:37 +0300

    u

--- b.sh
+++ b.sh
@@ -2,8 +2,8 @@
 
 #perl letopis.bld.pl compile -t nov_2020
 
-perl letopis.bld.pl compile -t vojna
-#perl letopis.bld.pl compile -t vojna.volonter
+#perl letopis.bld.pl compile -t vojna
+perl letopis.bld.pl compile -t vojna.volonter
 #perl letopis.bld.pl compile -t vojna.herson
 #perl letopis.bld.pl compile -t vojna.harkov
 #perl letopis.bld.pl compile -t poetry

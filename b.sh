350a0f4fa54c81d6a04224e4bc91e21b5a59b51a
me <me@localhost>
2022-05-18 15:07:14 +0300

    u

--- b.sh
+++ b.sh
@@ -5,5 +5,9 @@
 #perl letopis.bld.pl compile -t vojna
 #perl letopis.bld.pl compile -t vojna.volonter
 #perl letopis.bld.pl compile -t vojna.herson
-perl letopis.bld.pl compile -t vojna.harkov
+#perl letopis.bld.pl compile -t vojna.harkov
 #perl letopis.bld.pl compile -t poetry
+
+#prj-get-img -a ~/Documents/lts/rashizm/ok_ru/special
+prj-get-img -c db_add_md5
+#prj-lts -a db_img_md5 -c run

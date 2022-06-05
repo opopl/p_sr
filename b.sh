347fc418874b87e55bff82bec20b253fedbb2b2b
me <me@localhost>
2022-06-05 22:12:22 +0300

    u

--- b.sh
+++ b.sh
@@ -7,8 +7,9 @@
 #perl letopis.bld.pl compile -t vojna.herson
 #perl letopis.bld.pl compile -t vojna.harkov
 #perl letopis.bld.pl compile -t poetry
-perl letopis.bld.pl compile -t ki
+#perl letopis.bld.pl compile -t ki
 
 #prj-get-img -a ~/Documents/lts/rashizm/ok_ru/special
 #prj-get-img -c db_add_md5
 #prj-lts -a db_img_md5 -c run
+prj-lts -c run -a author_db_pages_util

ef1a25daad9940fdef01666d1a6680911348a1bc
me <me@localhost>
2022-11-13 10:25:24 +0200

    u

--- gi.sh
+++ gi.sh
@@ -12,6 +12,9 @@ export script="$PLG/projs/scripts/bufact/tex/get_img.pl"
 #export file="$P_SR/letopis.12_09_2021.fb.semesjuk_ivan.1.chelovek_prividenie.tex"
 
 #export file="letopis.24_12_2021.stz.news.ru.kp.1.doneck_jolka.pic.1.tex"
-export sec="29_01_2022.fb.druzenko_gennadiy.1.chy_bude_vijna"
+#export sec="29_01_2022.fb.druzenko_gennadiy.1.chy_bude_vijna"
+#export sec="29_01_2022.fb.druzenko_gennadiy.1.chy_bude_vijna"
+
+export sec="topics.vojna.my.12.profil.friends.tetjana_rudenko.chat"
 export file="letopis.$sec.tex"
-perl -d $script -f $file --img_root aa
+perl $script -f $file --img_root img_root

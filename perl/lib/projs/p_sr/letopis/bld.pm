966ef46aa7cd250b75b19185f7185c3bde7a5c7d
me <me@localhost>
2022-11-13 19:51:39 +0200

    u

--- perl/lib/projs/p_sr/letopis/bld.pm
+++ perl/lib/projs/p_sr/letopis/bld.pm
@@ -65,19 +65,41 @@ sub act_scr {
     foreach my $friend (@friends) {
         my $fsec = sprintf(q{%s.%s}, $root_sec, $friend);
         my $fdir = catfile($root_dir, $friend);
+        my $flines = [];
+        push @$flines,
+            '','\ifcmt',' import',
+            ' @path ' . $fdir,
+            ' @match p.png',
+            sprintf(' @tags scrn,profil,demidov,ok_ru,%s,profil_img',$friend),
+            '\fi';
 
         my $sd = $bld->_sec_data({ 
            proj => $proj,
            sec  => $fsec,
         });
-        next if !$sd || $bld->_sec_exist({ sd => $sd });
-        $DB::single = 1;
-        1;
-        $bld->sec_insert_child({
-           sec   => $root_sec,
-           proj  => $proj,
-           child => $fsec,
-        });
+
+        unless($sd && $bld->_sec_exist({ sd => $sd })){
+            print qq{creating child section: $fsec} . "\n";
+            $DB::single = 1;
+
+            $bld->sec_new({
+               proj  => $proj,
+               parent => $root_sec,
+               sec => $fsec,
+            });
+ 
+            $bld->sec_insert_child({
+               proj  => $proj,
+               sec   => $root_sec,
+               child => $fsec,
+            });
+
+            $bld->sec_insert({
+               proj  => $proj,
+               sec => $fsec,
+               lines => $flines,
+            });
+        }
     }
 
     my @tags_a = qw( ok_ru chat );
